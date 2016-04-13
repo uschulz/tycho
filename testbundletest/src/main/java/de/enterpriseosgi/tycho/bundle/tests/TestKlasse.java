f66001b471d874811d567436a61723ec8b16a803
Uwe Schulz <git@enterprise-osgi.com>
2016-04-13 20:30:37 +0200

    inital commit of bundle folders

---
+++ testbundletest/src/main/java/de/enterpriseosgi/tycho/bundle/tests/TestKlasse.java
@@ -0,0 +1,14 @@
+package de.enterpriseosgi.tycho.bundle.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestKlasse {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
